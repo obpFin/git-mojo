7e8d342c009d220c22577ea4eb55087f185c397c
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-07-23 13:59:51 +0300

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/essential', '@vue/prettier'],
+  //plugins: ['html'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-const-assign': 'warn',
+    'no-this-before-super': 'warn',
+    'no-undef': 'warn',
+    'no-unreachable': 'warn',
+    'no-unused-vars': 'warn',
+    'constructor-super': 'warn',
+    'valid-typeof': 'warn'
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};

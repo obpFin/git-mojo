94c5e3de6cdd7a8277cf02292098030d8a014e67
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-07-23 16:16:46 +0300

    sidebar

--- src/components/Mojo.vue
+++ src/components/Mojo.vue
@@ -4,17 +4,34 @@
   </div>
   <div class="content">
     <div class="inner">
-      <h2>Team: Montel</h2>
+      <Sidebar />
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import Sidebar from "./Sidebar/Sidebar.vue";
+const sourceUrl = "https://api.github.com";
 export default {
   name: "Mojo",
+  components: {
+    Sidebar
+  },
   data: function() {
     return {};
+  },
+  methods: {
+    getUserData(userName) {
+      if (typeof userName === "string") {
+        fetch(`${sourceUrl}/userName`)
+          .then(stream => stream.json())
+          .then(data => {
+            console.log(data);
+          })
+          .catch(error => console.log(error));
+      }
+    }
   }
 };
 </script>
@@ -22,8 +39,9 @@ export default {
 <style scoped lang="sass">
   @import "../assets/sass/mixins/mixins"
   @import "../assets/sass/main"
+
   .main
-    @include inner-shadow
+    //@include inner-shadow
     height: 100vh
     margin: 120px
     background: $primary-lighter
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar">
+    <img src="../../assets/images/mojo.png" alt="mojo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="sass">
+  @import "../../assets/sass/main"
+  $width: 100px
+  img
+    width: $width
+    height: $width
+
+  .sidebar
+    height: 100%
+    position: absolute
+    width: $width
+    background: $primary-light
+</style>

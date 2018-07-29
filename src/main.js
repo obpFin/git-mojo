a995928744afaee5f1227dde6f32286943bec749
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-07-29 22:19:16 +0300

    vuex initial setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
 import "normalize.css";
 import "./assets/sass/main.sass";
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount("#app");

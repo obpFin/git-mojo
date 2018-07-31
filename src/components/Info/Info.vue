258f18979a97d2d03b422b0c2f2d85543a293558
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-07-31 11:50:58 +0300

    info page

--- src/components/Info/Info.vue
+++ src/components/Info/Info.vue
@@ -1,7 +1,13 @@
 <template>
   <transition name="fade">
   <div class="info">
-    <p>Info</p>
+    <h1>Git Mojo</h1>
+    <ul>
+      <hr class="vertical">
+      <li class="text">By <a href="https://github.com/obpFin/git-mojo" target="_blank">Oskari Peltonen </a>2018</li>
+      <li class="text">Made with <a href="https://vuejs.org"><img src="../../assets/logo.png" alt="Vue"></a></li>
+      <li class="text">Made in <a href="https://www.instagram.com/p/BlptbDQgtXq/?taken-by=obp_fi" target="_blank"><img src="../../assets/images/camera-retro.png" alt=""></a></li>
+    </ul>
   </div>
   </transition>
 </template>
@@ -20,5 +26,17 @@ export default {
   @import "../../assets/sass/mixins/mixins"
   .info
     @include absolutecenter()
+    text-align: center
+    h1, li
+      margin: 40px 0
+    ul
+      > li
+        a
+          color: $primary-red
+          text-decoration: none
+        img
+          width: 40px
+          height: auto
+          transform: translateY(15px)
 
 </style>

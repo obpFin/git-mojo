58d35dcc50ec074476fe2676ad14bc107ba34a2b
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-08-08 19:18:25 +0300

    Opponent

---
+++ src/components/Versus/Opponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    opponent
+  </div>
+</template>
+
+<script>
+export default {
+  name: "opponent"
+};
+</script>
+
+<style scoped lang="sass">
+  @import "../../assets/sass/main"
+  @import "../../assets/sass/mixins/mixins"
+</style>

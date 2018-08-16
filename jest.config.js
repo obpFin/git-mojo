67bd508427c2ac5c53bc444d4df16369872786f2
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-08-16 17:26:29 +0300

    jest & eslint configs

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
-    "^.+\\.jsx?$": "babel-jest"
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.js$": "babel-jest"
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1"
--- tests/unit/.eslintrc.js
+++ tests/unit/.eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
-  env: {
-    jest: true
-  },
-  rules: {
-    'import/no-extraneous-dependencies': 'off'
+  extends: 'airbnb-base',
+  globals: {
+    jest: true,
+    expect: true,
+    mockFn: true,
+    config: true,
+    afterEach: true,
+    beforeEach: true,
+    describe: true,
+    it: true,
+    runs: true,
+    waitsFor: true,
+    pit: true,
+    require: true,
+    xdescribe: true,
+    xit: true
   }
-}
+};

b3ec70ca2ad5e25ff49f9faafcead7683a6b806e
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-08-18 10:29:46 +0300

    opponent fetching state logic

--- src/store/modules/opponent.js
+++ src/store/modules/opponent.js
@@ -2,60 +2,60 @@ import { fetchUserData, fetchUserContribs } from "../../api/api";
 
 //initial state
 const state = {
-  opponents: [],
-  loading: false,
-  opponentNotFound: false
+  opponents: []
 };
 
 //actions
 const actions = {
   getOpponent({ commit, state }, userName) {
+    commit("eraseNotFoundOpponents");
     commit("setFetchOpponent", true);
-    if (!userName) {
-      commit("setFetchOpponent", false);
-      commit("setOpponentNotFound", true);
-    } else if (userName && state.opponents.opponentNotFound) {
-      commit("setOpponentNotFound", false);
-    }
     const opponentData = fetchUserData(userName);
     const opponentContribs = fetchUserContribs(userName);
-    Promise.all([opponentData, opponentContribs])
-      .then(
-        data => {
-          commit("setOpponent", {
-            ...data[0],
-            ...data[1]
-          });
-          console.log("OpponentData", state);
-        },
-        error => {
-          console.log(error);
-          commit("setOpponentNotFound", true);
-        }
-      )
-      .finally(() => {
-        commit("setFetchOpponent", false);
-      });
+    Promise.all([opponentData, opponentContribs]).then(
+      data => {
+        commit("setOpponent", {
+          ...data[0],
+          ...data[1]
+        });
+        console.log("OpponentData", state);
+      },
+      error => {
+        console.log(error);
+        commit("setOpponentNotFound", true);
+      }
+    );
   }
 };
 //mutations
 const mutations = {
   setOpponent(state, opponent) {
-    state.opponents = state.opponents.filter(opp => !opp.loading);
+    state.opponents = readyOpponents(state);
     state.opponents.push(opponent);
   },
+  eraseNotFoundOpponents(state) {
+    state.opponents = readyOpponents(state);
+  },
   setFetchOpponent(state, status) {
-    state.loading = status;
     if (status === true) {
       state.opponents.push({ loading: true });
     }
   },
   setOpponentNotFound(state, status) {
-    state.opponentNotFound = status;
+    const index = state.opponents.findIndex(obj => obj.loading);
+    if (index > -1) {
+      console.log("Index ", index);
+      state.opponents[index].loading = false;
+      state.opponents[index].notFound = true;
+    }
   }
 };
 //getters
 const getters = {};
+
+//helpers
+const readyOpponents = state =>
+  state.opponents.filter(opp => !opp.notFound && !opp.loading);
 
 export default {
   namespaced: false,

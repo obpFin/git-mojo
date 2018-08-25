2f27cff169f864c07715423b0db52087ce2b8c0e
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-08-25 16:29:59 +0300

    opponentLevel util func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-export const getFavoriteLang = repos => {
+const getFavoriteLang = repos => {
   const langs = repos.map(r => r.language);
   const favLang = (langs => {
     // https://codereview.stackexchange.com/questions/177962/find-the-most-common-number-in-an-array-of-numbers
@@ -20,3 +20,25 @@ export const getFavoriteLang = repos => {
 
   return favLang;
 };
+
+const opponentLevel = (userScore, opponentScore) => {
+  const LEVELS = {
+    EASY: 'easy',
+    MEDIUM: 'medium',
+    HARD: 'hard'
+  }
+
+  console.log('userScore:', userScore, 'opponentScore: ', opponentScore)
+  const diff = (opponentScore - userScore) / userScore;
+  console.log('diff: ', diff)
+  switch (diff) {
+    case (diff < 0):
+      return LEVELS.EASY
+    case (diff > 2):
+      return LEVELS.HARD
+    default:
+      return LEVELS.MEDIUM
+  }
+}
+
+export {getFavoriteLang, opponentLevel};

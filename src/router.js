6dd4655575ce0036eb84ba33ab5ba0a014333899
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-08-29 20:24:06 +0300

    results route

--- src/router.js
+++ src/router.js
@@ -1,34 +1,41 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import store from "./store";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import store from './store';
 
-import User from "./components/User/User.vue";
-import Info from "./components/Info/Info.vue";
-import Versus from "./components/Versus/Versus.vue";
+import User from './components/User/User.vue';
+import Info from './components/Info/Info.vue';
+import Versus from './components/Versus/Versus.vue';
+import Result from './components/Versus/Result.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: [
     {
-      path: "/",
-      redirect: "/user"
+      path: '/',
+      redirect: '/user'
     },
     {
-      path: "/user",
-      name: "user",
+      path: '/user',
+      name: 'user',
       component: User
     },
     {
-      path: "/info",
-      name: "info",
+      path: '/info',
+      name: 'info',
       component: Info,
       meta: { auth: true }
     },
     {
-      path: "/versus",
-      name: "versus",
+      path: '/versus',
+      name: 'versus',
       component: Versus,
+      meta: { auth: true }
+    },
+    {
+      path: '/versus/results',
+      name: 'results',
+      component: Result,
       meta: { auth: true }
     }
   ]
@@ -38,7 +45,7 @@ router.beforeEach((to, from, next) => {
   const authRequired = to.matched.some(route => route.meta.auth);
   const authed = store.getters.isLoggedIn;
   if (authRequired && !authed) {
-    next("/");
+    next('/');
   } else {
     next();
   }

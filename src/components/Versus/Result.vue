d41ad46d3164fb72c421b62530f9bbba0672abad
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-09-01 10:01:51 +0300

    versus results user dimensions

--- src/components/Versus/Result.vue
+++ src/components/Versus/Result.vue
@@ -4,7 +4,13 @@
     <div v-for="(r, index) in results" :key="r.id" :class="`place${index+1}`">
       <i>{{ index + 1 }}</i>
       <img :src="r.img" alt="img">
-      <p>{{ r.name }}</p>
+      <div class="result__info">
+        <p class="name">{{ r.name }}</p>
+        <p class="">
+          <span>{{ r.score }}</span>
+          points
+        </p>
+      </div>
     </div>
   </div>
   </transition>
@@ -50,15 +56,15 @@ export default {
 
   .result
     @include absolutecenter()
+    background: $primary-dark
+    height: 70%
     display: flex
     flex-direction: column
     justify-content: center
-    height: 100%
     margin-left: $sidebar-width / 2
-    background: $primary-lighter
-    padding: 0 $base-margin * 2
+    padding: 40px $base-margin * 2
     @media only screen and (max-width: 420px)
-      margin-left: $sidebar-width-small
+      margin-left: 0
       padding: 0 20px
     > div
       display: flex
@@ -67,30 +73,57 @@ export default {
           font-size: 65px
           color: #FFD700
           margin: 10px
+        img
+          width: 100px
+          height: 100px
+        > div p
+          font-size: 2.0em
       &.place2
+        margin-left: 20px
         i
           font-size: 45px
           color: #C0C0C0
           margin: 10px
+        img
+          width: 80px
+          height: 80px
+        > div p
+          font-size: 1.5em
       &.place3
+        margin-left: 40px
         i
           font-size: 30px
           color: #cd7f32
           margin: 10px
+        img
+          width: 60px
+          height: 60px
+        > div p
+          font-size: 1.2em
+
       &.place4
+        margin-left: 60px
         i
           font-size: 20px
           color: #ffffff
           margin: 10px
+        img
+          width: 40px
+          height: 40px
+        > div p
+          font-size: 1em
+
       img
-        width: 100px
-        height: 100px
         border-radius: 100px
         margin: 40px 0
       p
-        margin: auto 20px
-
-
-
-
+        margin: auto
+        margin-left: 20px
+        font-size: $text-xl
+        span
+          @include highlight()
+        &.name
+          font-size: 38px
+      > div
+        margin: auto
 </style>
